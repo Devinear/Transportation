de0b23ed3f1cca56ed7e673bffedeaa006a51f1c
Devinear <leech8757@gmail.com>
2020-02-05 15:58:13 +0900

    버스 정류장 번호 검색 화면에 Floating Action Button 추가

--- app/src/main/java/com/custom/transportation/ui/fragment/BusNumberFragment.kt
+++ app/src/main/java/com/custom/transportation/ui/fragment/BusNumberFragment.kt
@@ -1,37 +1,42 @@
 package com.custom.transportation.ui.fragment
 
+import android.app.AlertDialog
 import android.content.Context
+import android.content.DialogInterface
 import android.os.Bundle
+import android.text.InputFilter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import com.custom.transportation.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class BusNumberFragment : TabFragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
-            = inflater.inflate(R.layout.fragment_bus_num, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view : View = inflater.inflate(R.layout.fragment_bus_num, container, false)
+
+        view.findViewById<FloatingActionButton>(R.id.fab).setOnClickListener {
+            AlertDialog.Builder(context).apply {
+                setTitle(context.getString(R.string.search_bus_number))
+                setView(EditText(context).apply {
+                    filters = Array(1) {
+                        InputFilter { source, start, end, dest, dstart, dend ->
+                            if (source != null && source.matches("^[0-9-]".toRegex()))
+                                source
+                            else
+                                ""
+                        }
+                    }
+                })
+                setPositiveButton(context!!.getString(android.R.string.ok)) { dialog: DialogInterface?, which:Int -> }
+            }.create().run { show() }
+        }
+        return view
+    }
 
     override fun getTitle(context: Context) : String = context.getString(R.string.bus_number)
-
-    // 추후 버스 정류장 번호 검색 개발시 참고
-//    override val fabClickListener: View.OnClickListener = View.OnClickListener {
-//        AlertDialog.Builder(context).apply {
-//            setTitle(context.getString(R.string.search_bus_number))
-//            setView(EditText(context).apply {
-//                filters = Array(1) {
-//                    InputFilter { source, start, end, dest, dstart, dend ->
-//                        if (source != null && source.matches("^[0-9-]".toRegex()))
-//                            source
-//                        else
-//                            ""
-//                    }
-//                }
-//            })
-//            setPositiveButton(context!!.getString(android.R.string.ok)) { dialog: DialogInterface?, which:Int ->
-//            }
-//        }.create().run { show() }
-//    }
 
     companion object {
         private var instance : BusNumberFragment? = null
