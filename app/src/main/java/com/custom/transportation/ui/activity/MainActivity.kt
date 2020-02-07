c08fc2f9118678509a385c06e04060cfe42813bc
Devinear <leech8757@gmail.com>
2020-02-07 17:13:03 +0900

    불필요 주석 제거

--- app/src/main/java/com/custom/transportation/ui/activity/MainActivity.kt
+++ app/src/main/java/com/custom/transportation/ui/activity/MainActivity.kt
@@ -18,27 +18,6 @@ class MainActivity : AppCompatActivity() {
         val viewPager: ViewPager = findViewById(R.id.view_pager)
         viewPager.adapter = pagerAdapter
 
-
-//        tabs.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
-//            override fun onTabReselected(p0: TabLayout.Tab?) { }
-//
-//            override fun onTabUnselected(p0: TabLayout.Tab?) { }
-//
-//            override fun onTabSelected(p0: TabLayout.Tab?) {
-//                if(p0 == null) return
-//
-//                when(p0.position) {
-//                    MAIN_TAB.BUS.pos -> { curTabType = MAIN_TAB.BUS }
-//                    MAIN_TAB.SUBWAY.pos -> { curTabType = MAIN_TAB.SUBWAY }
-//                    else -> { curTabType = MAIN_TAB.HOME }
-//                }
-//
-//                val fragment = pagerAdapter.getItem(curTabType.pos)
-//                fab.setImageDrawable(fragment.getDrawable(applicationContext))
-//                fab.setOnClickListener(fragment.fabClickListener)
-//            }
-//        })
-
         findViewById<TabLayout>(R.id.tabs)?.run {
             addOnTabSelectedListener(object :TabLayout.OnTabSelectedListener {
                 override fun onTabSelected(tab: TabLayout.Tab?) {
