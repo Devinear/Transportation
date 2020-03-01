6b06ec313d57a8a54ffd21b2bbb589069e6f679f
Chahyung Lee <leech8757@gmail.com>
2020-03-01 17:37:43 +0900

    Room Coroutine support 적용 - gradle

--- app/src/main/java/com/custom/transportation/repository/local/BookmarkDao.kt
+++ app/src/main/java/com/custom/transportation/repository/local/BookmarkDao.kt
@@ -5,20 +5,20 @@ import androidx.room.*
 @Dao
 interface BookmarkDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertBookmark(vararg bookmark: Bookmark)
+    suspend fun insertBookmark(vararg bookmark: Bookmark)
 
     @Delete
-    fun deleteBookmark(vararg bookmark: Bookmark)
+    suspend fun deleteBookmark(vararg bookmark: Bookmark)
 
     @Query("SELECT * FROM bookmarks WHERE id BETWEEN :minIndex AND :maxIndex")
-    fun getBetweenIndex(minIndex: Int, maxIndex: Int) : List<Bookmark>
+    suspend fun getBetweenIndex(minIndex: Int, maxIndex: Int) : List<Bookmark>
 
     @Query("SELECT * FROM bookmarks")
-    fun getAll(): List<Bookmark>
+    suspend fun getAll(): List<Bookmark>
 
     @Query("DELETE FROM bookmarks")
-    fun deleteAll()
+    suspend fun deleteAll()
 
     @Update
-    fun update(vararg bookmark: Bookmark)
+    suspend fun update(vararg bookmark: Bookmark)
 }
