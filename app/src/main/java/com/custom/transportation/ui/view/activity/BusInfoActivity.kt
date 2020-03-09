ef595da74dae4ac5ad018f58b3e2a5c1545b7943
Devinear <leech8757@gmail.com>
2020-03-09 16:37:15 +0900

    Bus Info의 Recycler 당겨서 새로고침 동작 추가 > SwipeRefreshLayout

--- app/src/main/java/com/custom/transportation/ui/view/activity/BusInfoActivity.kt
+++ app/src/main/java/com/custom/transportation/ui/view/activity/BusInfoActivity.kt
@@ -5,6 +5,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.custom.transportation.R
 import com.custom.transportation.common.IntentType
 import com.custom.transportation.ui.adapter.recycler.BusInfoAdapter
@@ -20,6 +21,8 @@ class BusInfoActivity : AppCompatActivity(), BusInfoContract.View {
     private val presenter: BusInfoContract.Presenter = BusInfoPresenter(this)
     private val busInfoAdapter = BusInfoAdapter(presenter)
 
+    private lateinit var swipeLayout: SwipeRefreshLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bus_info)
@@ -31,20 +34,32 @@ class BusInfoActivity : AppCompatActivity(), BusInfoContract.View {
 
         val arsId : String? = intent.getStringExtra(IntentType.ArsID.type)
 
-        findViewById<FloatingActionButton>(R.id.fab).apply {
-            isEnabled = arsId != null
-        }.setOnClickListener {
-            arsId ?: return@setOnClickListener
+        swipeLayout = findViewById(R.id.la_swipe)
+        swipeLayout.setOnRefreshListener {
+            arsId ?: return@setOnRefreshListener
             presenter.run { search(search = arsId) }
         }
+
+//        findViewById<FloatingActionButton>(R.id.fab).apply {
+//            isEnabled = arsId != null
+//        }.setOnClickListener {
+//            arsId ?: return@setOnClickListener
+//            presenter.run { search(search = arsId) }
+//        }
 
         // 최초 1회 검색 필요
         arsId ?: return
         presenter.run { search(search = arsId) }
     }
 
-    override fun searchSuccess() = busInfoAdapter.addItems(presenter.getData())
+    override fun searchSuccess() {
+        Toast.makeText(this, "Refreshing", Toast.LENGTH_SHORT).show()
+        busInfoAdapter.addItems(presenter.getData())
+        swipeLayout.isRefreshing = false
+    }
 
-    override fun searchFailure(msg: String)
-            = Toast.makeText(this, "Failure:${msg}", Toast.LENGTH_SHORT).show()
+    override fun searchFailure(msg: String) {
+        Toast.makeText(this, "Failure:${msg}", Toast.LENGTH_SHORT).show()
+        swipeLayout.isRefreshing = false
+    }
 }
